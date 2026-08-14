598e99bebbdbfc9fbbefdb7dfba1ff381729272d
gabrieldametto-sketch <gabriel.dametto@escola.pr.gov.br>
2026-08-14 11:41:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-const botao = document.querySelector(botao);
-const texto = document.getElementById('texto');
+const botoes = document.querySelectorAll(".botao");
 
-const frases = {
-    '1': 'Estudar cursos na Alura',
-    '2': 'Criar projetos em Javascript',
-    '3': 'Criar um portfolio',
-    '4': 'Atualizar meu currículo'
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+  };
 }
-
-botoes.forEach(botao => {
-    botao.addEventListener('click', () => {
-        // Busca a frase correspondente ao ID ou usa uma mensagem padrão
-        texto.textContent = frases[botao.id] || 'Nenhuma opção selecionada';
-    });
-});
